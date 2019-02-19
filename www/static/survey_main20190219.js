96da6280e6a7c5d7e25190cdfdf9c6142e87f0f1
nadira.nuri <nadira.nuri@gmail.com>
2019-02-19 12:12:57 +0600

    t 1 20190219

--- www/static/survey_main20190219.js
+++ www/static/survey_main20190219.js
@@ -482,7 +482,6 @@ function check_user() {
    		
 		
 	//$("#error_login").html(apipath+'check_user?cid='+localStorage.cid+'&cm_id='+localStorage.cm_id+'&cm_pass='+localStorage.cm_pass+'&synccode='+localStorage.synccode);	
-	alert (apipath+'check_user?cid='+localStorage.cid+'&cm_id='+localStorage.cm_id+'&cm_pass='+localStorage.cm_pass+'&synccode='+localStorage.synccode)
    		$.ajax({
 				 type: 'POST',
 				 url: apipath+'check_user?cid='+localStorage.cid+'&cm_id='+localStorage.cm_id+'&cm_pass='+localStorage.cm_pass+'&synccode='+localStorage.synccode,
